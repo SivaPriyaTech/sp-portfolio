62f1ec639fd7d283f7688624efc13bccb7655949
AbishECESIS <abishecesis@gmail.com>
2025-03-17 10:30:51 +0530

    slider section style corrections updated

--- js/script.js
+++ js/script.js
@@ -52,12 +52,13 @@ $(".mob.ab-pfo-nav-list-container .ab-pfo-nav-list-item").click(() => {
 
 // Skills Slider
 $(".ab-pfo-tech-skills-slick-slider").slick({
-  slidesToShow: 6,
-  infinite: true,
+  vertical:true,
+  slidesToShow: 1,
+  infinite: false,
   slidesToScroll: 1,
-  autoplay: true,
+  autoplay: false,
   autoplaySpeed: 1500,
-  dots: false,
+  dots: true,
   arrows: true,
   prevArrow: `<button type="button" class="slick-prev"><svg width="55" height="55" viewBox="0 0 55 55" fill="none" xmlns="http://www.w3.org/2000/svg">
   <circle cx="27.5" cy="27.5" r="27.5" fill="#F3E04F"/>
@@ -68,39 +69,7 @@ $(".ab-pfo-tech-skills-slick-slider").slick({
   <circle cx="27.5" cy="27.5" r="27.5" fill="#F3E04F"/>
   <path d="M42.0607 30.0607C42.6464 29.4749 42.6464 28.5251 42.0607 27.9393L32.5147 18.3934C31.9289 17.8076 30.9792 17.8076 30.3934 18.3934C29.8076 18.9792 29.8076 19.9289 30.3934 20.5147L38.8787 29L30.3934 37.4853C29.8076 38.0711 29.8076 39.0208 30.3934 39.6066C30.9792 40.1924 31.9289 40.1924 32.5147 39.6066L42.0607 30.0607ZM15 30.5H41V27.5H15V30.5Z" fill="black"/>
   </svg>
-  </button>`,
-  responsive: [
-    {
-      breakpoint: 1441,
-      settings: {
-        slidesToShow: 5,
-      },
-    },
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 4,
-      },
-    },
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 4,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-    {
-      breakpoint: 426,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-  ],
+  </button>`
 });
 
 // Academic Background section
